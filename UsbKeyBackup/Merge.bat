a855b214346c26c8328806ed03144543598a1f6c
TaoK <tao@klerks.biz>
2012-09-02 06:32:52 -0400

    Added submodule reference, fixed build process (somewhat)

--- UsbKeyBackup/Merge.bat
+++ UsbKeyBackup/Merge.bat
@@ -1,13 +1,20 @@
-"c:\Program Files\Microsoft\ILMerge\ILMerge.exe" /t:winexe /out:%1NewUsbKeyBackup.exe %1UsbKeyBackup.exe %1EasyProgressDialog.dll %1es\UsbKeyBackup.resources.dll
+"..\..\..\BuildTools\ILRepack\ILRepack.exe" /t:winexe /out:%1NewUsbKeyBackup.exe %1UsbKeyBackup.exe %1EasyProgressDialog.dll %1es\UsbKeyBackup.resources.dll
+IF %ERRORLEVEL% NEQ 0 GOTO END
+
 del %1EasyProgressDialog.dll
 del %1EasyProgressDialog.pdb
+del %1EasyProgressDialog.xml
 del %1UsbKeyBackup.exe
 del %1UsbKeyBackup.pdb
 del %1es\*.* /Q
 rmdir %1es
+
 copy %1UsbKeyBackup.exe.config %1NewUsbKeyBackup.exe.config
 del %1UsbKeyBackup.exe.config
+
 rem copy %1NewUsbKeyBackup.exe %1UsbKeyBackup.exe
 rem copy %1NewUsbKeyBackup.pdb %1UsbKeyBackup.pdb
 rem del %1NewUsbKeyBackup.exe
 rem del %1NewUsbKeyBackup.pdb
+
+:END
